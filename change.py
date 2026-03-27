e0f3645c0198035dbc3165ee4f5a5dd7a1170356
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-03-27 11:10:20 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,7 +2,6 @@ def change():
     """Lee un gasto y el dinero recibido, calcula el vuelto
     y lo separa en pesos (parte entera) y centavos.
     """
-    pass
     print("Ingresar gasto:")
     gasto = float(input("Ingresar gasto:\n"))
     print (gasto)
