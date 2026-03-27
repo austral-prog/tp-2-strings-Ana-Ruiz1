0ce09065f1153fee3c11cd93e01f5383c08b42ec
Ana-Ruiz1 <apruizmanzur@mail.austral.edu.ar>
2026-03-27 11:11:13 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,7 +2,6 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
     nombre = input("Introduce tu nombre: ")
     apellido = input("Introduce tu apellido: ")
     nombre_completo = nombre + " " + apellido
